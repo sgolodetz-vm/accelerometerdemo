1a527d6b9773e86b99335962fdf4df1d40dfacc8
Stuart Golodetz <stuart.golodetz@visionarymachines.com>
2022-12-02 15:12:24 +0000

    Add a getAccel function.

--- bar.js
+++ bar.js
@@ -1,4 +1,15 @@
 export function printBar() {
-    console.log("Bar");
+	console.log("Bar");
+}
+
+export function getAccel() {
+	DeviceMotionEvent.requestPermission().then(response => {
+		if(response == 'granted') {
+			console.log("Yes");
+		}
+		else {
+			console.log("No");
+		}
+	});
 }
 
